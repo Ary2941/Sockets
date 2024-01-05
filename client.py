3829fa6cb0231aaa2f333e1175048bcfe2c265a8
Amaury2941 <amaury2941@gmail.com>
2024-01-05 18:39:00 -0300

    utilizando de POO
    Todo: server enviar as mensagens

--- client.py
+++ client.py
@@ -1,5 +1,44 @@
 import socket,threading, sys
 
+class client:
+    def __init__(self,address):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.address = ('localhost',int(address))
+        self.socket.connect(self.address)
+
+    def send_message(self):
+        while 1:
+            mensagem = input("")
+            if mensagem == "!quit":
+                self.socket.close()
+                sys.exit()
+            try:
+                self.socket.send(mensagem.encode())
+
+            except(ConnectionResetError):
+                self.socket.close()
+                sys.exit()
+
+    def get_message(self):
+        while 1:
+            try:
+                mensagem = self.socket.recv(2143)
+                print(f'({self.address[1]})',mensagem.decode())
+            except(ConnectionAbortedError):
+                break        
+            except ConnectionError:
+                print("servidor fechou!")
+                sys.exit()
+
+
+    def run(self):
+        thread1 = threading.Thread(target=self.send_message, args=())
+        thread2 = threading.Thread(target=self.get_message, args=())
+
+        thread1.start()
+        thread2.start()
+
+'''
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 endereco_servidor=('localhost',2143)
@@ -19,8 +58,6 @@ def send_message():
             server.close()
             sys.exit()
 
-thread1 = threading.Thread(target=send_message, args=())
-thread1.start()
 
 
 def get_message():
@@ -34,5 +71,15 @@ def get_message():
             print("servidor fechou!")
             sys.exit()
 
+
+thread1 = threading.Thread(target=send_message, args=())
+
+
 thread2 = threading.Thread(target=get_message, args=())
+
+thread1.start()
 thread2.start()
+
+'''
+
+client(2143).run()
--- server.py
+++ server.py
@@ -1,5 +1,53 @@
 import socket, threading , sys
 
+
+
+class server:
+    def __init__(self,address):
+        self.socket  = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.address = ('localhost',int(address))
+        self.connections = []
+
+
+    def hear(self,entity,entity_name,connections_index):
+        while 1:
+            entity_response = entity.recv(self.address[1])
+
+            print(f"{entity_name} disse: {entity_response.decode()}")
+
+
+    def listen(self):
+        self.socket.bind(self.address)
+        self.socket.listen()
+        print(f'{self.address[0]} waiting connections on {self.address[1]}...')
+
+        while 1:
+            cliente, cliente_sitio = self.socket.accept()
+            
+            self.connections += [(cliente,cliente_sitio,threading.Thread(target=self.hear, args=(cliente,cliente_sitio[1],len(self.connections)-1)).start() )] #o socket, o endereço do mesmo, e uma thread que recebe as mensagens do socket
+            print(f"connection {len(self.connections)} stablished with {self.connections[len(self.connections)-1][1]}")
+
+    def broadcast(self):
+        while 1:
+            mensagem = input("")
+            if mensagem == "!quit":
+                pass
+
+            for conexao in self.connections:
+                conexao[0].send(mensagem.encode())
+
+
+    def run(self):
+        listenthread = threading.Thread(target=self.listen)
+        broadcastthread = threading.Thread(target=self.broadcast)
+
+        listenthread.start()
+        broadcastthread.start()
+
+server(2143).run()
+
+
+'''
 myself = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 address = ('localhost',2143)
 
@@ -73,3 +121,4 @@ thread3.join()
 
 
 
+'''
