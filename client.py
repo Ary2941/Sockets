fd6bf430496bcd1a916f15646063136017f7b89a
Amaury2941 <amaury2941@gmail.com>
2024-01-16 17:42:21 -0300

    Update client.py

--- client.py
+++ client.py
@@ -9,48 +9,10 @@ class client:
         self.address = ('localhost',int(address))
         self.socket.connect(self.address)
         self.messages = []
-        self.channels = {"todos":[]}
-
-        ## interface
-        self.column1 = []
+        self.channels = {"todos":["",[]]}
         self.current_channel = "todos"
         self.channel_to_send = "todos"
-        
-        layout = [ 
-            [
-                [sg.Text(f"Usuário{self.socket_name}",key="USERNAME")],
-            ],
-            
-            [ 
-                sg.Table(
-                    self.column1,
-                    headings=["Conversas"],
-                    size=(5, 6),
-                    enable_events=True,
-                    justification='center',
-                    enable_click_events=True,
-                    key ="CONVERSAS",
-                    ),
-                                    
-                sg.Column([
-                        [sg.Text("Canal atual: todos",key="CURRENTCHANNELNAME")],
-                        [sg.Listbox(self.channels[self.current_channel],key='CHANNELVIEW',size=(30, 4), font=('Arial Bold', 14))]
-                    ])
-            ], 
 
-            [
-                sg.Input(size=(20, 1), font=('Arial Bold', 14), expand_x=True, key='INPUT'),
-                sg.Button("OK"),
-            ],
-                [
-                                
-                            ],
-                            [
-                                sg.Text("",key='STATUS'),
-                            ],
-        ]
-                            
-        self.view = sg.Window(title="Cliente",layout=layout, margins=(100, 50))
 
     def send_message(self):
 
@@ -65,25 +27,26 @@ class client:
                 
                 message = values['INPUT']
 
-                self.view['STATUS'].update(f"enviando a mensagem para o canal {self.channel_to_send}")
-                
-                self.channels[self.channel_to_send].append(f"eu: {message}")
+                self.channels[self.channel_to_send][1].append(f"eu: {message}")
                 
                 self.socket.send(f'{self.channel_to_send} {message}'.encode())
 
                 self.view['INPUT'].update("")
-                self.view['CHANNELVIEW'].update(self.channels[self.channel_to_send])
-                print(self.channels)
+                self.view['CHANNELVIEW'].update(self.channels[self.channel_to_send][1])
+                self.view['CHANNELVIEW'].Widget.yview('moveto', '1.0')
                 
 
             if '+CLICKED+' in event:
                 if event[2][0] is not None: 
-                    print("changing channel to:{} ".format(self.column1[event[2][0]]))
-                    self.current_channel = self.column1[event[2][0]]
-                    self.channel_to_send = self.column1[event[2][0]]
+                    channel_clicked = list(self.channels.keys())[event[2][0]]
+                    self.channels[channel_clicked][0] = ""
+                    self.current_channel = channel_clicked
+                    self.channel_to_send = channel_clicked
                     self.view['INPUT'].update("")
-                    self.view['CHANNELVIEW'].update(self.channels[self.current_channel])
-                    self.view['CURRENTCHANNELNAME'].update(f"Canal atual: {self.column1[event[2][0]]}")
+                    self.view['CHANNELVIEW'].update(self.channels[self.current_channel][1])
+                    self.view['CURRENTCHANNELNAME'].update(f"Canal atual: {channel_clicked}")
+                    self.view['CONVERSAS'].update([[channelname, self.channels[channelname][0]] for channelname in list(self.channels.keys())])
+
 
             if event == "L1":
                 channel_to_send = values[event][0][0]
@@ -102,23 +65,15 @@ class client:
 
                     ## criando o channels da interface
                     for channel in mensagem.decode().split(" ")[1].split(",")[1:]:
-                        self.channels[channel] = []
-                        self.column1.append(channel)
-                    print(self.channels)
-                
-                    ### 
+                        self.channels[channel] = ["",[]]
 
                 # algum cliente entrou
                 elif mensagem.decode().split(" ")[0] == "link_start":
-                    self.channels[mensagem.decode().split(" ")[1]] = []
-                    self.column1.append(mensagem.decode().split(" ")[1])
+                    self.channels[mensagem.decode().split(" ")[1]] = ["",[]]
 
-                    self.view['CONVERSAS'].update(self.column1)
+                    self.view['CONVERSAS'].update([[channelname, self.channels[channelname][0]] for channelname in list(self.channels.keys())])
                     self.current_channel = "todos"
-                    self.view['CHANNELVIEW'].update(self.channels[self.current_channel])
-
-
-                    print(self.channels)
+                    self.view['CHANNELVIEW'].update(self.channels[self.current_channel][1])
 
                 # algum cliente saiu
                 elif mensagem.decode().split(" ")[0] == "link_lost":
@@ -127,29 +82,26 @@ class client:
                     
                     if self.current_channel == mensagem.decode().split(" ")[1]:
                         self.current_channel = 'todos'
-                        self.view['CHANNELVIEW'].update(self.channels[self.current_channel])
-                    
-                    self.column1.remove(mensagem.decode().split(" ")[1])
-                    
-                    self.view['CONVERSAS'].update(self.column1)
-
-                    print(self.channels)
+                        self.view['CHANNELVIEW'].update(self.channels[self.current_channel][1])
+                                        
+                    self.view['CONVERSAS'].update([[channelname, self.channels[channelname][0]] for channelname in list(self.channels.keys())])
 
                 # recebeu alguma mensagem em alguns do channels
                 elif mensagem.decode().split(": ")[0] in self.channels.keys():
-                    self.channels[mensagem.decode().split(": ")[0]].append(": ".join(mensagem.decode().split(": ")[1:]))
+                    self.channels[mensagem.decode().split(": ")[0]][1].append(": ".join(mensagem.decode().split(": ")[1:]))
                     
                     print(": ".join(mensagem.decode().split(": ")[1:]))
 
                     att = mensagem.decode().split(": ")[0]
 
-                    print(f"Cchanel{self.current_channel}")
+                    if self.current_channel == att:
+                        self.view['CHANNELVIEW'].update(self.channels[att][1])
+                        self.view['CHANNELVIEW'].Widget.yview('moveto', '1.0')
 
-                    if self.current_channel == mensagem.decode().split(": ")[0]:
-                        self.view['CHANNELVIEW'].update(self.channels[mensagem.decode().split(": ")[0]])
-
-
-                    print(self.channels)
+                    elif self.current_channel != att:
+                        self.channels[att][0] = "+"
+                        self.view['STATUS'].update(f"Mensagem recebida na conversa com {att}")
+                    self.view['CONVERSAS'].update([[channelname, self.channels[channelname][0]] for channelname in list(self.channels.keys())])
 
                 else:
                     print(f"servidor: {mensagem.decode()}")
@@ -161,6 +113,42 @@ class client:
                 sys.exit()
 
     def run(self):
+
+
+        layout = [ 
+            [
+                [sg.Text(f"Usuário{self.socket_name}",key="USERNAME")],
+            ],
+            
+            [ 
+                sg.Table(
+                    self.channels,
+                    headings=["Conversas"," + "],
+                    size=(5, 6),
+                    enable_events=True,
+                    justification='center',
+                    enable_click_events=True,
+                    key ="CONVERSAS",
+                    ),
+                                    
+                sg.Column([
+                        [sg.Text("Canal atual: todos",key="CURRENTCHANNELNAME")],
+                        [sg.Listbox(self.channels[self.current_channel][1],key='CHANNELVIEW',auto_size_text=True,size=(30, 10), font=('Arial Bold', 14))]
+                    ])
+            ], 
+
+            [
+                sg.Input(size=(20, 1), font=('Arial Bold', 14), expand_x=True, key='INPUT'),
+                sg.Button("OK"),
+            ],
+                [
+                                
+                            ],
+                            [
+                                sg.Text("",key='STATUS'),
+                            ],
+        ]
+        self.view = sg.Window(title="Cliente",layout=layout, margins=(1, 1))
         thread1 = threading.Thread(target=self.send_message, args=())
         thread2 = threading.Thread(target=self.get_message, args=())
 
